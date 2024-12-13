b8dc343debcf20d657f3639f4182e5e0d92a9f9c
Evan <evan.pittman@keyin.com>
2024-12-13 19:23:07 -0330

    added PageController so the page renders

--- src/main/java/com/keyin/domain/binarysearchtree/BinarySearchTreeController.java
+++ src/main/java/com/keyin/domain/binarysearchtree/BinarySearchTreeController.java
@@ -1,6 +1,7 @@
 package com.keyin.domain.binarysearchtree;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
